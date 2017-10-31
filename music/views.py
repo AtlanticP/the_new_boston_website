49cbf159538e05df1cf3bd9abe5b869bac8a9ab4
office <davidovnv@gmail.com>
2017-10-31 14:12:13 +0300

    part 13. Connecting to database - creating list of albums (views.index HttpResponse (iteration))

--- music/views.py
+++ music/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Album
+
+from .models import Album 
 
 def index(self):
-	return HttpResponse('<h1>Hellow, World!</h1>')
+	all_albums = Album.objects.all()
+	html = ''
+	for album in all_albums:
+		url = '/music/' + str(album.id) + '/'
+		html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+	return HttpResponse(html)
 
-def details(request, album_id): # !!!!!!!! new
+def details(request, album_id):
 	return HttpResponse("<h2>Details for album id: " + str(album_id) + '<h2>')
