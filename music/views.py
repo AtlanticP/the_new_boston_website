13889383237fac3b2b39294ea6ea101efe2d6b52
office <davidovnv@gmail.com>
2017-10-31 16:08:40 +0300

    templates, raising a 404 Http error

--- music/views.py
+++ music/views.py
@@ -6,12 +6,11 @@ from .models import Album
 
 def index(request):
 	all_albums = Album.objects.all()
-	html = ''
-	context = {'all_albums' : all_albums}
-	return render(request, 'app/index.html', context)
+	return render(request, 'app/index.html', {'all_albums' : all_albums})
 
 def details(request, album_id):
 	try:
 		album = Album.objects.get(pk=album_id)
 	except:
 		raise Http404("Album does not exist")
+	return render(request, 'app/details.html', {'album' : album})
