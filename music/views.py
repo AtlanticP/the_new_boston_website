1a4a0b9462f400b34f70ae5503c98d0abcbcece7
office <davidovnv@gmail.com>
2017-10-31 14:48:20 +0300

    render template (send all_albums iterable object - Album.objects.all() to 'index.html'

--- music/views.py
+++ music/views.py
@@ -4,13 +4,11 @@ from .models import Album
 
 from .models import Album 
 
-def index(self):
+def index(request):
 	all_albums = Album.objects.all()
 	html = ''
-	for album in all_albums:
-		url = '/music/' + str(album.id) + '/'
-		html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-	return HttpResponse(html)
+	context = {'all_albums' : all_albums}
+	return render(request, 'app/index.html', context)
 
 def details(request, album_id):
 	return HttpResponse("<h2>Details for album id: " + str(album_id) + '<h2>')
