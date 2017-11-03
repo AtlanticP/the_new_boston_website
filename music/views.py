feee7a5bca76caca18e6b0ed1a4ff7a10ca88d3b
office <davidovnv@gmail.com>
2017-11-03 13:59:58 +0300

    24 Favorite Button, Favorite View Function, Adding Forms to the Template

--- music/views.py
+++ music/views.py
@@ -1,6 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render,get_object_or_404
 
 from .models import Album 
 
@@ -10,8 +8,19 @@ def index(request):
 	return render(request, 'music/index.html', {'all_albums' : all_albums})
 
 def details(request, album_id):
-	try:
-		album = Album.objects.get(pk=album_id)
-	except:
-		raise Http404("Album does not exist")
+	album = get_object_or_404(Album, pk=album_id)
 	return render(request, 'music/details.html', {'album' : album})
+
+def favorite(request, album_id):
+	album = get_object_or_404(Album, pk=album_id)
+	try:
+		selected_song = album.song_set.get(pk=request.POST['song'])
+	except (KetyError, Song.DoesNotExist):
+		return render(request, 'music/details.html', {
+			'album': album,
+			'error_message': "you did not select a valid song",
+			})
+	else:
+		selected_song.is_favorite = True
+		selected_song.save()
+		return render(request, 'music/details.html', {'album': album})
