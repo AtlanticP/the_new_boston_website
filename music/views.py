3a5c1acf9c1fbbeab7c2a0a5ef97e1292bd90d8e
office <davidovnv@gmail.com>
2017-11-07 16:30:27 +0300

    Generic views

--- music/views.py
+++ music/views.py
@@ -1,26 +1,13 @@
-from django.shortcuts import render,get_object_or_404
+from django.views import generic
+from .models import Album
 
-from .models import Album 
+class IndexView(generic.ListView):
+	template_name = 'music/index.html'
+	# context_object_name = 'list_of_albums'
 
+	def get_queryset(self):
+		return Album.objects.all()
 
-def index(request):
-	all_albums = Album.objects.all()
-	return render(request, 'music/index.html', {'all_albums' : all_albums})
-
-def details(request, album_id):
-	album = get_object_or_404(Album, pk=album_id)
-	return render(request, 'music/details.html', {'album' : album})
-
-def favorite(request, album_id):
-	album = get_object_or_404(Album, pk=album_id)
-	try:
-		selected_song = album.song_set.get(pk=request.POST['song'])
-	except (KetyError, Song.DoesNotExist):
-		return render(request, 'music/details.html', {
-			'album': album,
-			'error_message': "you did not select a valid song",
-			})
-	else:
-		selected_song.is_favorite = True
-		selected_song.save()
-		return render(request, 'music/details.html', {'album': album})
+class DetailView(generic.DetailView):
+	model = Album
+	template_name = 'music/detail.html'
