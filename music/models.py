e086dac15b830fd1fd7457350092a1f11b75bcce
office <davidovnv@gmail.com>
2017-11-09 18:15:11 +0300

    generic: create album, update and delete

--- music/models.py
+++ music/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Album(models.Model):
 
@@ -6,6 +7,9 @@ class Album(models.Model):
 	album_title = models.CharField(max_length=500)
 	genre = models.CharField(max_length=1000)
 	album_logo = models.CharField(max_length=500, default='')
+
+	def get_absolute_url(self):
+		return reverse('music:details', kwargs={'pk': self.pk})
 
 	def __str__(self):
 		return self.artist + ' - ' + self.album_title
@@ -16,7 +20,7 @@ class Song(models.Model):
 	album = models.ForeignKey(Album, on_delete=models.CASCADE)
 	file_type = models.CharField(max_length=10)
 	song_title = models.CharField(max_length=250)
-	is_favorite = models.BooleanField(default=False) #!!!!! new
 
 	def __str__(self):
 		return self.song_title
+
